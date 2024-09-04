9413075bdcf14ef87912335b547e96ddd96f1907
hexaalif <shadullahsad9@gmail.com>
2024-09-04 22:04:52 +0600

    dark mode added

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -1,21 +1,29 @@
-import {
-  MdDarkMode,
-  MdOutlineFileDownload,
-  MdPhoneIphone,
-} from "react-icons/md";
+import { MdOutlineFileDownload, MdPhoneIphone } from "react-icons/md";
 import two from "../images/crop.jpg";
 import { RiMailSendFill } from "react-icons/ri";
 import { ImLocation } from "react-icons/im";
 import { FaFacebookSquare, FaGithub, FaLinkedin } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import lgo from "../assets/logo.png";
+import { useTheme } from "../context/useTheme";
 
 const Banner = () => {
+  const { theme, lightMode, darkMode } = useTheme();
+
   const handleDownloadClick = () => {
     window.open(
       "https://drive.google.com/file/d/1_cdjfbe5UMUBxIa6R_jvCFGppjPgaXpT/view",
       "_blank"
     );
+  };
+
+  const changeTheme = (e) => {
+    const darkChecked = e.currentTarget.checked;
+    if (darkChecked) {
+      darkMode();
+    } else {
+      lightMode();
+    }
   };
 
   return (
@@ -47,9 +55,28 @@ const Banner = () => {
                 </span>
                 <p className="text-xs">Sylhet, Bangladesh</p>
               </div>
-              <span className="text-3xl text-white">
-                <MdDarkMode />
-              </span>
+              <div className="flex items-center space-x-4 ml-2">
+                {/* <button
+                  onClick={changeTheme}
+                  className="text-xl bg-gray-700 p-3 rounded-full"
+                  value={theme === "dark"}
+                >
+                  <MdDarkMode />
+                </button> */}
+                <label className="relative inline-flex items-center cursor-pointer">
+                  <input
+                    type="checkbox"
+                    value=""
+                    className="sr-only peer"
+                    onChange={changeTheme}
+                    checked={theme === "dark"}
+                  />
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-gray-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600"></div>
+                  {/* <span className="ml-3 text-sm font-medium text-gray-900">
+                    Toggle Theme
+                  </span> */}
+                </label>
+              </div>
             </div>
           </div>
           {/* <hr /> */}
--- src/context/useTheme.js
+++ src/context/useTheme.js
@@ -11,3 +11,20 @@ export const ThemeProvider = themeContext.Provider;
 export function useTheme() {
   return useContext(themeContext);
 }
+
+// const ThemeContext = createContext();
+
+// export const useTheme = () => useContext(ThemeContext);
+
+// export const ThemeProvider = ({ children }) => {
+//   const [theme, setTheme] = useState("light");
+
+//   const lightMode = () => setTheme("light");
+//   const darkMode = () => setTheme("dark");
+
+//   return (
+//     <ThemeContext.Provider value={{ theme, lightMode, darkMode }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// };
